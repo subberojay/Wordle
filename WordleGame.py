ccbdb3976499bc305e9f044ecd85804828e8afd1
subberojay <daniel.c.carter@outlook.com>
2024-03-06 17:12:08 +0000

    Added settings, can now change dictionary, game mode, number of guesses displayed

--- WordleGame.py
+++ WordleGame.py
@@ -15,7 +15,7 @@ class wordleGame:
         self.accepts = accepts
         
         
-    def giveGuesses(self):
+    def giveGuessesHard(self):
         if self.dictionary == 'small':
             wordsFile = 'Files\wordle-answers-alphabetical.txt'
             lookupTableFile = 'Files\lookup-table.txt'
@@ -27,8 +27,7 @@ class wordleGame:
             table =  open(lookupTableFile, 'r')
             lookup_table = table.readlines()
             table.close()
-            
-            if (self.guesses, self.answers) == ([''], ['']):
+            if (self.guesses, self.answers) == ([], []):
                 scores = []
                 for i in [words.index('trace'), 
                           words.index('least'),
@@ -65,10 +64,74 @@ class wordleGame:
                         patterns[lookup_table[i][x * 5 : (x + 1) * 5]] = [x]
                 avg_words = 0
                 for p in patterns:
-                    avg_words += len(patterns[p])**2
+                    if p != 'GGGGG':
+                        avg_words += len(patterns[p])**2
                 avg_words /= len(accepts)
                 scores.append((words[i], avg_words, patterns))
                 scores.sort(key = lambda x: x[1])                    
+            return scores
+    
+    def giveGuessesNormal(self):
+        if self.dictionary == 'small':
+            wordsFile = 'Files\wordle-answers-alphabetical.txt'
+            lookupTableFile = 'Files\lookup-table.txt'
+        else:
+            wordsFile = 'Files\sgb-words.txt'
+            lookupTableFile = 'Files\lookup-table-sgb.txt'
+        with open(wordsFile, 'r') as file:
+            words = [w[:5] for w in file.readlines()]
+            table =  open(lookupTableFile, 'r')
+            lookup_table = table.readlines()
+            table.close()
+            if (self.guesses, self.answers) == ([], []):
+                scores = []
+                for i in [words.index('trace'), 
+                          words.index('least'),
+                          words.index('stale'),
+                          words.index('crate'),
+                          words.index('slant')]:
+                    patterns = {}
+                    for x in range(len(words)):
+                        try:
+                            patterns[lookup_table[i][x * 5 : (x + 1) * 5]].append(x)
+                        except: 
+                            patterns[lookup_table[i][x * 5 : (x + 1) * 5]] = [x]
+                    scores.append((words[i], None, patterns))
+                return scores
+            if self.accepts == []:
+                accepts = range(len(words))
+                for (n, guess) in enumerate(self.guesses):
+                    new_accepts = []
+                    i = words.index(guess)
+                    answer = self.answers[n]
+                    for x in accepts:
+                        if lookup_table[i][x * 5 : (x + 1) * 5] == answer:
+                            new_accepts.append(x)
+                    accepts = new_accepts.copy()
+            else:
+                accepts = self.accepts
+            scores = []
+            for (i, w) in enumerate(words):
+                patterns = {}
+                for x in accepts:
+                    try:
+                        patterns[lookup_table[i][x * 5 : (x + 1) * 5]].append(x)
+                    except: 
+                        patterns[lookup_table[i][x * 5 : (x + 1) * 5]] = [x]
+                avg_words = 0
+                for p in patterns:
+                    if p != 'GGGGG':
+                        avg_words += len(patterns[p])**2
+                avg_words /= len(accepts)
+                scores.append((w, avg_words, patterns))
+            scores.sort(key = lambda x: x[1])
+            if scores[0][1] < 1:
+                newScores = []
+                n = 0
+                while scores[n][1] < 1:
+                    newScores.append(scores[n])
+                    n += 1
+                scores = newScores
             return scores
         
 
--- WordleSolver.py
+++ WordleSolver.py
@@ -1,58 +1,106 @@
 
 from WordleGame import wordleGame
 
-def solveWordle(wordle):
+def solveWordle(wordle, gameMode = 'hard', guessesDisplayed = 5):
     if type(wordle) != wordleGame:
         raise TypeError('solveWordle must take a wordleGame as argument')
+    if wordle.dictionary == 'small':
+        wordsFile = 'Files\wordle-answers-alphabetical.txt'
+    elif wordle.dictionary == 'large':
+        wordsFile = 'Files\sgb-words.txt'
     game, answer = wordle, None
-    while answer != 'GGGGG':
-        scores = game.giveGuesses()
-        guesses = [score[0] for score in scores] 
-        guessesDisplayed = 5
-        print(guesses[:guessesDisplayed])
-        # while True == True:
-        #     moreGuesses = input('Show more guesses [Y/N]?: ')
-        #     while moreGuesses != 'Y' and moreGuesses != 'N':
-        #         moreGuesses = input('Show more guesses [Y/N]?: ')
-        #     if moreGuesses == 'N':
-        #         break
-        #     if guessesDisplayed == 30:
-        #         print(guesses[:30])
-        #         print('max number of guesses to be displayed is 30')
-        #         break
-        #     else:
-        #         guessesDisplayed += 3
-        #         print(guesses[:guessesDisplayed])
-        
-        with open('Files\wordle-answers-alphabetical.txt', 'r') as file:
-            words = [w[:5] for w in file.readlines()]
-            guess = input('guess: ')
-            while guess not in words:
-                print('please enter a valid wordle solution')
+    if gameMode == 'hard':
+        while answer != 'GGGGG':
+            scores = game.giveGuessesHard()
+            guesses = [score[0] for score in scores] 
+            print(guesses[:guessesDisplayed])
+            with open(wordsFile, 'r') as file:
+                words = [w[:5] for w in file.readlines()]
                 guess = input('guess: ')
-                
-        answer = input('answer: ')
-        while len(answer) != 5 or set(answer) - {'X', 'Y', 'G'} != set():
-            print('please enter valid answer')
+                while guess not in words:
+                    print('guess not in current dictionary')
+                    guess = input('guess: ')
             answer = input('answer: ')
-        
-        if guess in guesses:
-            accepts = scores[guesses.index(guess)][2][answer]
-        else:
-            accepts = []
-        game = wordleGame([game.guesses] + [guess], 
-                          [game.answers] + [answer],
-                          game.dictionary,
-                          accepts)
+            while len(answer) != 5 or set(answer) - {'X', 'Y', 'G'} != set():
+                print('please enter valid answer')
+                answer = input('answer: ')
+            if guess in guesses:
+                accepts = scores[guesses.index(guess)][2][answer]
+            else:
+                accepts = []
+            game = wordleGame(game.guesses + [guess], 
+                              game.answers + [answer],
+                              game.dictionary,
+                              accepts)
+    elif gameMode == 'normal':
+        while answer != 'GGGGG':
+            scores = game.giveGuessesNormal()
+            guesses = [score[0] for score in scores] 
+            print(guesses[:guessesDisplayed])
+            with open(wordsFile, 'r') as file:
+                words = [w[:5] for w in file.readlines()]
+                guess = input('guess: ')
+                while guess not in words:
+                    print('guess not in current dictionary')
+                    guess = input('guess: ')
+            answer = input('answer: ')
+            while len(answer) != 5 or set(answer) - {'X', 'Y', 'G'} != set():
+                print('please enter valid answer')
+                answer = input('answer: ')
+            if guess in guesses:
+                accepts = scores[guesses.index(guess)][2][answer]
+            else:
+                accepts = []
+            game = wordleGame(game.guesses + [guess], 
+                              game.answers + [answer],
+                              game.dictionary,
+                              accepts)
   
 if __name__ == "__main__":
-    print('Please enter your guesses so far as a comma separated list, e.g. grain, after, spear. If you are solving a new game prior to making any guesses, leave the prompt blank and hit enter.')
+    dictSize, gameMode, guessesDisplayed = 'large', 'hard', 5
+    print('Please enter your guesses so far as a comma separated list, e.g. grain, after, spear. If you are solving a new game prior to making any guesses, leave the prompt blank and hit enter. You can also view and change settings options by entering "settings".')
     guesses = input('guesses: ')
-    print('\nNow give the responses given by Wordle, again as a comma separated list. For each guess, please give a five character string. The colours given to a letter by Wordle should be inputted as follows: ')
-    print('grey = X, yellow = Y, green = G')
-    print('So if "after" was guessed and f, t were not in the hidden word, a and e were, but we had them in the wrong place and r was in the word and was in the right place, one would input YXXYG')
+    while guesses == 'settings':
+        print('Settings that may be altered are:'
+              '\n\nDictionary size - This can either be "small" or "large". Small contains only the 2315 that are potential wordles. Rarer words, plurals and more will never be the solution to a wordle and are excluded from this dictionary. Large contains 5755 words, but still not all five letter words. The default size is large.'
+              '\nGame mode - This can be "normal" or "hard". Wordle offers both regular and "Hard Mode" in its settings. In Wordle\'s Hard Mode, only guesses that fit the current pattern may be given. For example if we\'ve guessed TRACE and the T has turned yellow, our next guess must contain a T. Hard mode here obeys the same convention. The default mode is hard. Note that the solver runs quicker in hard mode, as it has less words to check.'
+              '\nGuesses displayed - This governs the maximum amount of suggestions the solver will give at a time. The default setting is 5.')
+        settingToChange = None
+        while settingToChange != '':
+            settingToChange = input('Setting to change (leave blank to exit): ')
+            if settingToChange.casefold() == 'dictionary size':
+                dictSize = None
+                while dictSize not in ['small', 'large']:
+                    print('Please select either "small" or "large" dictionary size.')
+                    dictSize = input('Dictionary size: ').casefold()
+            elif settingToChange.casefold() == 'game mode':
+                gameMode = None
+                while gameMode not in ['normal', 'hard']:
+                    print('Please select either "normal" or "hard" game mode.')
+                    gameMode = input('Game mode: ').casefold()
+            elif settingToChange.casefold() == 'guesses displayed':
+                guessesDisplayed = None
+                isInt = False
+                while isInt is False:
+                    print('Please give an integer value for the number of guesses to be displayed.')
+                    guessesDisplayed = input('Guesses displayed: ')
+                    try:
+                        guessesDisplayed = int(guessesDisplayed)
+                        isInt = True
+                    except ValueError:
+                        isInt = False
+            elif settingToChange != '':
+                print('Please enter "Dictionary size", "Game mode", or "Guesses displayed" to change one of these settings. Leave blank to exit.')
+        guesses = input('guesses: ')
+    print('\nNow give the responses given by Wordle, again as a comma separated list. For each guess, please give a five character string. The colours given to a letter by Wordle should be inputted as follows: '
+    '\n\n    grey = X, yellow = Y, green = G'
+    '\n\nSo if "after" was guessed and f, t were not in the hidden word, a and e were, but we had them in the wrong place and r was in the word and was in the right place, we would input YXXYG')
     answers = input('answers: ')
     guesses = [g.strip() for g in guesses.split(',')]
     answers = [a.strip() for a in answers.split(',')]
-    solveWordle(wordleGame(guesses, answers))
+    if guesses == ['']:
+        guesses = []
+    if answers == ['']:
+        answers = []
+    solveWordle(wordleGame(guesses, answers, dictSize), gameMode, guessesDisplayed)
         
